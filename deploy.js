9c6cb8089470f70a168dc49884802aa4cc20029f
lidiaCana <in.canaless@gmail.com>
2024-02-09 00:13:07 +0000

    deploy: added deploy script

--- deploy.js
+++ deploy.js
@@ -1,5 +1,17 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
 //updated web3 and hdwallet-provider imports added for convenience
-
+const { abi, bytecode } = require("./compile");
 // deploy code will go here
+
+const provider = new HDWalletProvider("mneomonic phares", "infura link api");
+const web3 = new Web3(provider);
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: 1000000, from: accounts[0] });
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
